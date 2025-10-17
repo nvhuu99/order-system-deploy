c8ae7fe3449f0b8f355a973ab989eecae1af0be9
huu.nv <nvhuu.business@gmail.com>
2025-10-17 22:35:50 +0700

    add node_group instance type t3.small

--- nodes.tf
+++ nodes.tf
@@ -13,7 +13,7 @@ resource "aws_eks_node_group" "general" {
   }
 
   capacity_type  = "ON_DEMAND"
-  instance_types = ["t3.medium"]
+  instance_types = ["t3.small", "t3.medium"]
 
   scaling_config {
     desired_size = 1
